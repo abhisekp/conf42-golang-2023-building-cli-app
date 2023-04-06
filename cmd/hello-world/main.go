582eed7c9730fe4176ec52617e997d861b747181
Abhisek Pattnaik <abhisekp@engineer.com>
2023-04-06 15:55:49 +0530

    Help flag

--- cmd/hello-world/main.go
+++ cmd/hello-world/main.go
@@ -9,20 +9,33 @@ import (
 )
 
 func main() {
-	name := flag.String("name", "", "The name to say hello to.")
-	// flag.StringVar(&name, "n", "", "The name to say hello to.")
+	var name string
+	flag.StringVar(&name, "name", "", "The name to say hello to.")
+	flag.StringVar(&name, "n", "", "The name to say hello to.")
+	var repeat int
+	flag.IntVar(&repeat, "repeat", 1, "The number of times to repeat.")
+
+	totalArgs := len(os.Args[1:])
+
+	var help bool
+	flag.BoolVar(&help, "help", totalArgs == 0, "Show help")
+
 	getopt.Aliases(
 		"n", "name",
+		"r", "repeat",
+		"h", "help",
 	)
 
 	getopt.Parse()
 
-	if name != nil && *name == "" {
+	if help {
 		getopt.PrintDefaults()
 		os.Exit(0)
 	}
 
-	if name != nil {
-		fmt.Println("Hello,", *name)
+	if name != "" {
+		for i := 0; i < repeat; i++ {
+			fmt.Println("Hello,", name)
+		}
 	}
 }
