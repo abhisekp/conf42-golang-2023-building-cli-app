b910861ce3480ff229ca5d6e2d818a39a2639037
Abhisek Pattnaik <abhisekp@engineer.com>
2023-04-06 22:36:14 +0530

    getopt naming cli app

--- cmd/hello-world/main.go
+++ cmd/hello-world/main.go
@@ -2,40 +2,30 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"os"
 
 	"rsc.io/getopt"
 )
 
 func main() {
-	var name string
-	flag.StringVar(&name, "name", "", "The name to say hello to.")
-	flag.StringVar(&name, "n", "", "The name to say hello to.")
-	var repeat int
-	flag.IntVar(&repeat, "repeat", 1, "The number of times to repeat.")
-
 	totalArgs := len(os.Args[1:])
 
-	var help bool
-	flag.BoolVar(&help, "help", totalArgs == 0, "Show help")
+	f := getopt.NewFlagSet("Abhisek's CLI", flag.ExitOnError)
 
-	getopt.Aliases(
-		"n", "name",
-		"r", "repeat",
+	var help bool
+	f.BoolVar(&help, "help", totalArgs == 0, "Show help")
+
+	f.Aliases(
 		"h", "help",
 	)
 
-	getopt.Parse()
+	err := f.Parse(os.Args[1:])
+	if err != nil {
+		return
+	}
 
 	if help {
 		getopt.PrintDefaults()
 		os.Exit(0)
-	}
-
-	if name != "" {
-		for i := 0; i < repeat; i++ {
-			fmt.Println("Hello,", name)
-		}
 	}
 }
