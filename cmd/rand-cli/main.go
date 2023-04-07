c6167130faebd25291f6ccf10a13d0cec954e2d2
Abhisek Pattnaik <abhisekp@engineer.com>
2023-04-07 20:43:19 +0530

    Random data with fakeit

--- cmd/rand-cli/main.go
+++ cmd/rand-cli/main.go
@@ -1,36 +1,36 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"math/rand"
-	"os"
 	"time"
 
-	"rsc.io/getopt"
+	"github.com/brianvoe/gofakeit/v6"
+	"github.com/davecgh/go-spew/spew"
+	"github.com/hexops/valast"
 )
 
+type Address struct {
+	Street  string
+	City    string
+	Pincode string
+	State   string
+	Country string
+}
+
 func main() {
-	min, max := 0, 1
+	seed := time.Now().Unix()
+	faker := gofakeit.New(seed)
 
-	flag.IntVar(&min, "min", min, "Minimum value")
-	flag.IntVar(&max, "max", max, "Maximum value")
+	fakeAddr := faker.Address()
 
-	getopt.Aliases(
-		"m", "min",
-		"M", "max",
-	)
-
-	getopt.Parse()
-
-	if max <= min {
-		fmt.Println("max must be greater than min")
-		os.Exit(1)
+	addr := Address{
+		Street:  fakeAddr.Street,
+		City:    fakeAddr.City,
+		Pincode: fakeAddr.Zip,
+		State:   fakeAddr.State,
+		Country: fakeAddr.Country,
 	}
 
-	// Seed the random number generator with the current time
-	rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
-
-	randomVal := min + rnd.Intn(max-min)
-	fmt.Println(randomVal)
+	spew.Dump(addr)
+	fmt.Println(valast.String(addr))
 }
