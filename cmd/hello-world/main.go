636dbe8386439d67c8b3d4cfaa53b07f5e967638
Abhisek Pattnaik <abhisekp@engineer.com>
2023-04-07 11:16:52 +0530

    parse string passed to flag

--- cmd/hello-world/main.go
+++ cmd/hello-world/main.go
@@ -4,68 +4,24 @@ import (
 	"errors"
 	"flag"
 	"fmt"
-	"strings"
+	"regexp"
+	"strconv"
 )
 
-type Address struct {
-	Street  string
-	City    string
-	State   string
-	Pincode string
-	Country string
-}
-
-type AddressValue struct {
-	*Address
-}
-
-func (a AddressValue) Get() any {
-	return a.Address
-}
-
-func (a AddressValue) String() string {
-	if a.Address == nil {
-		return ""
-	}
-	return fmt.Sprintf("%s, %s, %s, %s, %s", a.Address.Street, a.Address.City, a.Address.State, a.Address.Pincode, a.Address.Country)
-}
-
-func (a AddressValue) Set(addressStr string) error {
-	addressParts := strings.Split(addressStr, ", ")
-	if len(addressParts) == 0 || len(addressParts) > 5 {
-		return errors.New("Address should be in the format: Street, City, State, Pincode, Country")
-	}
-
-	if len(addressParts) >= 1 {
-		a.Address.Street = addressParts[0]
-	}
-
-	if len(addressParts) >= 2 {
-		a.Address.City = addressParts[1]
-	}
-
-	if len(addressParts) >= 3 {
-		a.Address.State = addressParts[2]
-	}
-
-	if len(addressParts) >= 4 {
-		a.Address.Pincode = addressParts[3]
-	}
-
-	if len(addressParts) >= 5 {
-		a.Address.Country = addressParts[4]
-	}
-
-	return nil
-}
+var reNum = regexp.MustCompile(`\d+`)
 
 func main() {
-	var address Address
-	addressVar := AddressValue{&address}
-	flag.Var(&addressVar, "address", "The address of the person in the format: Street, City, State, Pincode, Country")
+	var num int
+	flag.Func("num", "number of records to generate", func(s string) error {
+		var err error
+		num, err = strconv.Atoi(reNum.FindString(s))
+		if err != nil {
+			return errors.New("string must have number")
+		}
+		return nil
+	})
 
 	flag.Parse()
 
-	fmt.Println("Address")
-	fmt.Printf("%q\n", address)
+	fmt.Println("num:", num)
 }
