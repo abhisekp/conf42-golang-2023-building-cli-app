3d93ad4aa9d2de22172ee52ed2309d95af52bedd
Abhisek Pattnaik <abhisekp@engineer.com>
2023-04-07 20:59:41 +0530

    Random data with fakeit struct

--- cmd/rand-cli/main.go
+++ cmd/rand-cli/main.go
@@ -10,25 +10,30 @@ import (
 	"github.com/hexops/valast"
 )
 
-type Address struct {
-	Street  string `fake:"{street}"`
-	City    string `fake:"{city}"`
-	Pincode string `fake:"{zip}"`
-	State   string `fake:"{state}"`
-	Country string `fake:"{country}"`
+type Student struct {
+	Name       string
+	Age        int
+	Percentage float32
+	Height     int
+	Active     bool
+	Meta
+}
+
+type Meta struct {
+	Description string
 }
 
 func main() {
 	seed := time.Now().Unix()
 	faker := gofakeit.New(seed)
 
-	var addr Address
-	err := faker.Struct(&addr)
+	var stu Student
+	err := faker.Struct(&stu)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	spew.Dump(addr)
-	fmt.Println(valast.String(addr))
+	spew.Dump(stu)
+	fmt.Println(valast.String(stu))
 }
