2d37125d42d8cdf3362d0b44737937fffa0c668a
Abhisek Pattnaik <abhisekp@engineer.com>
2023-04-08 09:18:01 +0530

    Concurrent Gen multiple random person data

--- internal/cmd/person.go
+++ internal/cmd/person.go
@@ -1,15 +1,23 @@
 package cmd
 
 import (
+	"sync"
+
 	"github.com/abhisekp/conf42-golang-2023-building-cli-app/pkg/person"
 )
 
 // GenPersons generates a `n` number of Person(s)
 func GenPersons(n int) []*person.Person {
 	persons := make([]*person.Person, n)
+	wg := sync.WaitGroup{}
 	for i := 0; i < n; i++ {
-		p := person.NewPerson()
-		persons[i] = p
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			p := person.NewPerson()
+			persons[i] = p
+		}(i)
 	}
+	wg.Wait()
 	return persons
 }
